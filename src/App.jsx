6952050bcd155d2cf53227ce5b314a3728e96093
Fatima Schera <fatima.schera@gmail.com>
2024-01-11 09:50:08 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Footer } from "./views/Footer/Footer";
+import { Header } from "./views/Header/Header";
+
+const App = () => (
+  <>
+    <Header></Header>
+    <Footer></Footer>
+  </>
+);
+
+export default App;
---
+++ src/views/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+export const Footer = () => (
+  <footer>
+    <div>Footer</div>
+  </footer>
+);
